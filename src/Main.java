488fbab4092fd6c4f236de9cab6817ba2a38aabb
Daniel <danielcamposrocha9@gmail.com>
2022-10-07 12:40:59 +0200

    Arranco Proyecto, la idea es pasar lo de pseudocodigo a Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+   System.out.print(primo());
     }
+
+    /*este primer metodo obtiene los numeros primos menores que 100*/
+    public static String primo() {
+
+        System.out.println ("1");
+        System.out.println ("2");
+        for (int i=3;i<100;i=i+2) {
+            int n=3;
+            int c=0;
+            while (n<i) {
+                if (i%n== 0){
+                    c=c+1;
+                }
+                n=n+2;
+            }
+            if (c == 0){
+                System.out.println(i+" es un número primo");
+            }
+        }
+        return "estos son los números primos menores que 100";
+    }
+
+    
 }
