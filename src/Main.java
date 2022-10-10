4343470745b66ede9bb101b3cee522bb92c9c8cc
Daniel <danielcamposrocha9@gmail.com>
2022-10-10 23:20:44 +0200

    se han incluido nuevos metodos fin, areatriangulo y dividirmayormenor. introducirNnumero() tanto para double como para int

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
            System.out.println("pulse 3 método entredos");
            System.out.println("pulse 4 método números primos");
            System.out.println("pulse 5 método validar fecha");
+           System.out.println("pulse 6 método fin");
+           System.out.println("pulse 7 método área triangulo");
+           System.out.println("pulse 8 método dividir mayor menor");
            c = sc.nextInt();
            switch (c) {
                case 1 -> tabla();
@@ -19,11 +22,27 @@ public class Main {
                case 3 -> entredos();
                case 4 -> primo();
                case 5 -> validarfecha();
+               case 6 -> fin();
+               case 7 -> areaTriamgulo();
+               case 8 ->dividirMayorMenor();
            }
        }while(c!=0);
 
 
     }
+    private static int introducirNnumero (int i){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduzca un número entero");
+        int entero=sc.nextInt();
+        return entero;
+    }
+    private static double introducirNnumero (double i){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduzca un número, el decimal con coma");
+        double entero=sc.nextDouble();
+        return entero;
+    }
+
 /*se valida una fecha no se tienen en cuenta los bisiestos*/
     private static void validarfecha() {
         int dia,mes,anho;
@@ -63,14 +82,16 @@ public class Main {
 
     /*dos múmeros*/
     private static void entredos() {
-        Scanner sc=new Scanner(System.in);
+        //Scanner sc=new Scanner(System.in);
         int mayor,menor,pares,num1,num2,pimpares;
         pares=0;
         pimpares=1;
-        System.out.println("Introduzca un número");
+        /*System.out.println("Introduzca un número");
         num1=sc.nextInt();
         System.out.println("Introduzca un número");
-        num2=sc.nextInt();
+        num2=sc.nextInt();*/
+        num1 =introducirNnumero (1);
+        num2=introducirNnumero (1);
         if(num1>num2){
             mayor=num1-1;
             menor=num2+1;
@@ -96,10 +117,11 @@ public class Main {
         int n=0;
         int mayor=0;
         int menor=999999;
-        Scanner sc=new Scanner(System.in);
+        //Scanner sc=new Scanner(System.in);
         while (n!=-1){
-            System.out.println("introduzca un número");
-            n=sc.nextInt();
+            /*System.out.println("introduzca un número");
+            n=sc.nextInt(); */
+            n=introducirNnumero (2);
             if (n>=mayor){
                 mayor=n;
             }
@@ -140,6 +162,41 @@ public class Main {
         }
 
     }
+    // en el double reocrdar porner decimales con coma no con punto
+    private static void dividirMayorMenor() {
+        //Scanner sc= new Scanner(System.in);
+        /*System.out.println("introduzca numero");
+        double num1 = sc.nextDouble();
+        System.out.println("introduzca otro numero");
+        double num2 = sc.nextDouble();*/
+        double num1= introducirNnumero(2.1);
+        double num2=introducirNnumero(2.1);
+        if(num2>num1) {
+            System.out.println(" la división  de "+num2+" entre "+num1+" es: " + (num2 / num1));
+        }else {System.out.println(" la división  de "+num1+" entre "+num2+" es: "+(num1/num2));}
+    }
 
+    //cálculo del área de un triángulo
+    private static void areaTriamgulo() {
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("introduzca la altura del triangulo");
+        double altura = sc.nextDouble();
+        System.out.println("introduzca la base del triangulo");
+        double base = sc.nextDouble();
+        System.out.println("El área del triangulo es: "+(base*altura)/2);
+    }
+
+    /* Ejercicio 1 del tema 5*/
+    private static void fin() {
+        String teclado;
+        Scanner sc= new Scanner(System.in);
+        do {
+            System.out.println("introduzca lo que quiera, cuando quiera salir ponga FIN");
+            teclado = sc.nextLine();
+            System.out.println(teclado);
+        }while(!teclado.equals("FIN"));
+        System.out.println("Se cerró el programa");
+    }
 
 }
